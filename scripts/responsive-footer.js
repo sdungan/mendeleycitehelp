ac884c82dc2880f0525a612e1cfc5d0c98264ce1
sdungan <88577421+sdungan@users.noreply.github.com>
2021-08-08 07:42:24 -0400

    Add files via upload

--- scripts/responsive-footer.js
+++ scripts/responsive-footer.js
@@ -1,6 +1,7 @@
 /* Toggle between adding and removing the "responsive" class to the footer when you click the hamburger */
 function hamburger() {
   var x = document.getElementById("myFootnav");
+
   if (x.className === "footnav") {
     x.className += " responsive";
   } else {
